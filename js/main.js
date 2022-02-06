80b116235006f80ac6e373654aaedd52e8562ebf
Andrey Laptiev <andreylaptiev13@gmail.com>
2022-02-06 20:38:35 +0200

    Add script to change social icon color on hover
    Change image source when user hovers social link icon.
    And return default image when no hover.

--- js/main.js
+++ js/main.js
@@ -13,3 +13,30 @@ if (burgerMenuBtn) {
   });
 };
 
+// hover sidebar image links
+// names match html 'alt' attribute for each image
+const activeSocialImages = {
+  'instagram': 'img/instagram-icon-active.svg',
+  'facebook': 'img/facebook-icon-active.svg',
+  'vkontakte': 'img/vkontakte-icon-active.svg',
+  'youtube': 'img/youtube-icon-active.svg',
+}
+
+const defaultSocialImages = {
+  'instagram': 'img/instagram-icon.svg',
+  'facebook': 'img/facebook-icon.svg',
+  'vkontakte': 'img/vkontakte-icon.svg',
+  'youtube': 'img/youtube-icon.svg',
+}
+
+function activeImage(e) {
+  e.target.src = activeSocialImages[e.target.alt];
+};
+
+function defaultImage(e) {
+  e.target.src = defaultSocialImages[e.target.alt];
+};
+
+const socialImages = document.querySelectorAll('.header-side__social-image');
+socialImages.forEach(image => image.addEventListener('mouseover', activeImage));
+socialImages.forEach(image => image.addEventListener('mouseout', defaultImage));
